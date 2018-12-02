69b5769a9f105ba58e79f92bba9adbf88b0952f8
AthenaHe <athenahe126@qq.com>
2018-12-02 19:36:39 +0800

    加了注释

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -13,13 +13,13 @@ class Concentration//翻牌游戏
     var cards = [Card]() //定义一个cards数组，类型是Card
     var indexOfOneAndOnlyFaceUpCard: Int?
     
-    func chooseCard(at index:Int){
-        if !cards[index].isMatched{ //如果卡片么有匹配成功
-            if let matchIndex = indexOfOneAndOnlyFaceUpCard,matchIndex != index{
+    func chooseCard(at index:Int){ //选择卡牌动作
+        if !cards[index].isMatched{ //如果卡片没有匹配成功
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard,matchIndex != index{ //如果indexOfOneAndOnlyFaceUpCard！=index
                 //检查卡片是否朝上
                 if cards[matchIndex].identifier == cards[index].identifier { //如果在现在翻开的卡片和已经翻开的卡片的头像一样，说明匹配成功
-                    cards[matchIndex].isMatched = true //现在翻开的卡片匹配成功
-                    cards[index].isMatched = true //已经翻开的卡片匹配成功
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true //翻开卡片
                 indexOfOneAndOnlyFaceUpCard = nil
@@ -34,10 +34,10 @@ class Concentration//翻牌游戏
         }
     }
     
-    init(numberOfPairsOfCards:Int) { //numberOfPairsOfCards卡片对数的数量
+    init(numberOfPairsOfCards:Int) { //卡牌初始化，创建卡牌//numberOfPairsOfCards卡片对数的数量
         for _ in 1...numberOfPairsOfCards{
             let card = Card()   //创建两张标识符一样的卡片
-            cards += [card,card]
+            cards += [card,card] //把两张卡牌加到卡牌数组里面
             
         }
         //未完待续。。。。(翻牌随机太简单，需要设置复杂一些)
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -7,52 +7,52 @@
 //
 
 import UIKit
-
+//按住commad可以rename
  class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards : cardButtons.count+1 / 2 ) //卡牌对的数量
+    lazy var game = Concentration(numberOfPairsOfCards : cardButtons.count+1 / 2 ) //卡牌对的数量 ，设置成lazy属性是为了让它使用到的时候再初始化
     var flipCount: Int = 0 {
         didSet{
-            flipCountLabel.text = "flips : \(flipCount)"
+            flipCountLabel.text = "flips : \(flipCount)" //设置翻牌的次数
         }
     }
-    @IBOutlet weak  var flipCountLabel: UILabel!
-    @IBOutlet  var cardButtons: [UIButton]!  //UIBtton数组 //按住commad可以rename
-    @IBAction  func touchCard(_ sender: UIButton) {
-        flipCount += 1
-        if let cardNumber = cardButtons.index(of: sender) {
-            game.chooseCard(at: cardNumber)
-            upadateViewFromModel()
+    @IBOutlet weak  var flipCountLabel: UILabel! //展示翻牌次数
+    @IBOutlet  var cardButtons: [UIButton]!  //UIButton数组
+    @IBAction  func touchCard(_ sender: UIButton) { //点击卡牌事件
+        flipCount += 1 //翻牌次数加一
+        if let cardNumber = cardButtons.index(of: sender) { //每个卡牌数字号就是对应cardButtons下标
+            game.chooseCard(at: cardNumber) //游戏开始，点中一张卡牌
+            upadateViewFromModel() //更新视图
         }else{
             print("chosen card was not in cardButtons")
         }
         
     }
     
-    func upadateViewFromModel(){
-        for index in cardButtons.indices{ //根据卡片索引找到对应照片
+    func upadateViewFromModel(){ //更新视图
+        for index in cardButtons.indices{ //根据卡牌按钮索引找到对应卡牌
             let button = cardButtons[index]
             let card = game.cards[index]
             if card.isFaceUp{
-                //翻面（正面朝上）
+                //卡牌翻正面
                 button.setTitle(emoji(for:card),for: UIControlState.normal)
                 button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             }else{
-                //如果有匹配，就把匹配的两张消掉(设置成透明)，否则，这两张翻面(背景设为红色)
+                //消除卡牌(把卡牌设置成透明)
                 button.setTitle("", for: UIControlState.normal)
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0) : #colorLiteral(red: 1, green: 0.44582057, blue: 0.4437403977, alpha: 1)//color
                 
             }
         }
     }
-    var emojiChoices = ["🙈","🐶","🐭","🦁","🐷","🐰","🐙","🐸"]
-    var emoji = [Int:String]()
-    func emoji(for card:Card)->String{
+    var emojiChoices = ["🙈","🐶","🐭","🦁","🐷","🐰","🐙","🐸"] //各种头像集合
+    var emoji = [Int:String]() //表情字典
+    func emoji(for card:Card)->String{//设置卡片表情
             if emoji[card.identifier] == nil , emojiChoices.count > 0 {
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count))) //将卡牌打乱(随机设置卡牌上的表情)
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex) //将emojiChoices数组中的表情移除放到emoji数组中
             }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card.identifier] ?? "?" //有表情返回表情，表情用完了就返回问号
     }
 }
 
